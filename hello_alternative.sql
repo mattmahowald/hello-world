3b36cee0a163f3f45c7f8ceffefa061d463192d3
Matt Mahowald <mcm2018@stanford.edu>
2015-10-21 13:12:37 -0700

    Create hello_alternative.sql
    Created an alternative sql program to create a table with the "Hello, world!" string.

---
+++ hello_alternative.sql
@@ -0,0 +1,15 @@
+/* hello_alternative.sql
+ * ---------------------
+ * This program demonstrates an alternative method
+ * to selecting a "Hello, world!" string by first 
+ * defining a table.
+
+-- Creating the table
+DROP TABLE IF EXISTS Hello; 
+CREATE TABLE Hello(Greeting string);
+INSERT INTO Hello VALUES ("Hello, world!");
+
+-- Executing the query
+SELECT Greeting -- or * which selects all, as Hello has one attribute
+FROM Hello
+WHERE Hello.Greeting == "Hello, world!"
